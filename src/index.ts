74228cffc73c4ed6eb3b85ed9f3cfd0970ea61b2
DerekAtNen <derek@nencreative.com>
2024-12-17 17:21:59 -0800

    Prove of concept prototype

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
-import { greetUser } from '$utils/greet';
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import listPlugin from '@fullcalendar/list';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  const calendarElement = document.getElementById<HTMLDivElement>('calendar-container');
+  if (!calendarElement) return;
+
+  console.log(calendarElement);
+
+  const calendarEl = calendarElement;
+  const calendar = new Calendar(calendarEl, {
+    plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,listWeek',
+    },
+  });
+  calendar.render();
 });
